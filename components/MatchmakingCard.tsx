96b7c588559c3dac84bcae3412ba5a42dbc25b5c
Nicholas Rios <rios.nicholas@protonmail.com>
2024-10-13 22:11:49 -0400

    Add responsive design and media queries for MatchmakingCard

--- components/MatchmakingCard.tsx
+++ components/MatchmakingCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
+import { Cross1Icon, HandIcon } from "@radix-ui/react-icons";
 
 interface MatchmakingCardProps {
   name: string;
@@ -22,34 +23,42 @@ export default function MatchmakingCard({
   compact = false,
 }: MatchmakingCardProps) {
   return (
+
     <Card className="bg-secondary text-secondary-foreground">
-      <div className={`flex p-${compact ? 2 : 6} gap-4 justify-${compact ? 'start' : 'center'}`}>
-        <Avatar className={`h-${compact ? '12' : '20'} w-${compact ? '12' : '20'}`}>
-          <AvatarImage src="../../public/profile-picture.jpg" />
-          <AvatarFallback>{name}</AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col justify-center text-left">
-          <CardTitle>{name}, {age}</CardTitle>
-          <CardHeader className="p-0 font-medium">{university}</CardHeader>
+      <div className={`grid gap-4 lg:gap-1 ${compact ? 'lg:grid-cols-[2fr,3fr]' : 'grid-cols-1'} p-${compact ? '2' : '6'}`}>
+        <div className="flex items-center">
+          <Avatar className={`h-${compact ? '12' : '20'} w-${compact ? '12' : '20'} mr-4 lg:mr-2`}>
+            <AvatarImage src="../../public/profile-picture.jpg" />
+            <AvatarFallback>{name}</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col justify-center text-left">
+            <CardTitle className="lg:text-base">
+              {name}
+              {!compact && `, ${age}`}
+            </CardTitle>
+            <CardHeader className="lg:text-xs p-0 font-medium">{university}</CardHeader>
+          </div>
         </div>
+
+        <CardContent className={`flex flex-col justify-center items-center ${compact ? 'p-2 lg:p-0' : 'p-6'} lg:text-sm`}>
+          {!compact && (
+            <p className="text-center text-sm w-full">{intro}</p>
+          )}
+          <div className={`bg-primary text-primary-foreground ${compact ? "mt-0" : "mt-2"} rounded p-1 w-full`}>
+            {accountability}
+          </div>
+          <div className="bg-primary text-primary-foreground mt-2 rounded p-1 w-full">
+            {growth}
+          </div>
+        </CardContent>
       </div>
-      <CardContent className={`p-${compact ? 2 : 6}`}>
-        {!compact && (
-          <p className="text-center text-sm">{intro}</p>
-        )}
-        <div className="bg-primary text-primary-foreground mt-2 rounded p-1 ">
-          {accountability}
-        </div>
-        <div className="bg-primary text-primary-foreground mt-2 rounded p-1">
-          {growth}
-        </div>
-      </CardContent>
-      <div id="profile-actions" className={`flex justify-around p-${compact ? "1" : "4"}`}>
-        <Button variant="destructive" id="reject-button" className={`bg-white border-none rounded-full ${compact ? "w-10 h-10 text-xl" : "w-12 h-12 text-2xl"} cursor-pointer shadow-md hover:shadow-lg text-red-500 hover:text-white`}>
-          ✖
+
+      <div id="profile-actions" className={`flex justify-around ${compact ? 'p-1' : 'p-4'}`}>
+        <Button variant="destructive" id="reject-button" className={`bg-white border-none rounded-full ${compact ? 'w-10 h-10 text-xl' : 'w-12 h-12 text-2xl'} cursor-pointer shadow-md hover:shadow-lg text-red-500 hover:text-white`}>
+          <Cross1Icon/> 
         </Button>
-        <Button variant="default" id="connect-button" className={`bg-white border-none rounded-full w-12 h-12 text-2xl cursor-pointer shadow-md hover:shadow-lg text-green-500 ${compact ? "w-10 h-10 text-xl" : "w-12 h-12 text-2xl"}`}>
-          ✋ 
+        <Button variant="default" id="connect-button" className={`bg-white border-none rounded-full ${compact ? 'w-10 h-10 text-xl' : 'w-12 h-12 text-2xl'} cursor-pointer shadow-md hover:shadow-lg text-green-500 hover:text-white`}>
+          <HandIcon/> 
         </Button>
       </div>
     </Card>
