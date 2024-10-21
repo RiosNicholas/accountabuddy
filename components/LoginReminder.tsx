56792a5af082c0b69f3549016717af38f19734d9
Nicholas Rios <rios.nicholas@protonmail.com>
2024-10-21 14:43:16 -0400

    Create LoginReminder fallback component

---
+++ components/LoginReminder.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const LoginReminder: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[50vh] p-4 text-center">
+      <h2 className="text-xl font-bold text-primary mb-2">
+        Don't miss out!
+      </h2>
+      <p className="text-secondary-foreground text-lg font-medium">
+        Please <Link href="/auth/login" className="text-accent hover:underline">sign in</Link> to access all the features.
+      </p>
+    </div>
+  );
+};
+
+export default LoginReminder;
