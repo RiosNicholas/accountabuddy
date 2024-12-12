3a0a284cdb0e1e6aa2d5951d9b2b313817d00b34
Nicholas Rios <rios.nicholas@protonmail.com>
2024-12-12 16:41:59 -0500

    Add session validation to access UserSettings

--- app/settings/UserSettings.tsx
+++ app/settings/UserSettings.tsx
@@ -2,15 +2,24 @@
 
 import UserBio from "./UserInfo"
 import UserContact from "./UserContact"
+import LoginReminder from "@/components/LoginReminder"
+import { useSession } from "next-auth/react";
 
 export default function UserSettings() { 
+  const { data: session, status } = useSession();
   return (
-    <div className="flex flex-col justify-center items-start w-full md:w-3/4 lg:w-3/5 xl:w-2/5">
-      <h1 className="text-left text-xl font-bold mb-2">
-        Edit Profile
-      </h1>
-      <UserBio />
-      <UserContact />
-    </div>
+    <>
+    {session ? (
+      <div className="flex flex-col justify-center items-start w-full md:w-3/4 lg:w-3/5 xl:w-2/5">
+        <h1 className="text-left text-xl font-bold mb-2">
+          Edit Profile
+        </h1>
+        <UserBio />
+        <UserContact />
+      </div>
+    ) : (
+      <LoginReminder />
+    )}
+    </>
   )
 }
