1da8b72c2939bdaff93e880bfa83906ea3065ce0
Nicholas Rios <rios.nicholas@protonmail.com>
2024-12-13 23:52:58 -0500

    Add authentication validation for conditional rendering

--- app/settings/UserSettings.tsx
+++ app/settings/UserSettings.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import UserBio from "./UserInfo";
+import { useSession } from "next-auth/react";
+
+import UserInfo from "./UserInfo";
 import UserContact from "./UserContact";
+import UserSettingsLoading from "./UserSettingsLoading";
 import LoginReminder from "@/components/LoginReminder";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { useSession } from "next-auth/react";
-
 export default function UserSettings() {
   const { data: session, status } = useSession();
+  const name = "nick";
+  const biography = "I'm a software engineer and I love building";
+
+  if (status === "loading") return <UserSettingsLoading />;
   return (
     <>
-      {session ? (
+      {session && status === "authenticated" ? (
         <div className="flex flex-col justify-center items-start w-full md:w-3/4 lg:w-3/5 xl:w-2/5">
           <h1 className="text-left text-xl font-bold mb-2 w-4/5">
             Edit Profile
@@ -25,7 +30,7 @@ export default function UserSettings() {
                 </Avatar>
               </div>
             </div>
-            <UserBio />
+            <UserInfo name={name} biography={biography}/>
             <UserContact />
           </main>
         </div>
