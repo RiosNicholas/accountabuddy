5b66c28f2be4c259583352d231b3eb9fcb9d131d
Nicholas Rios <rios.nicholas@protonmail.com>
2024-12-16 14:21:30 -0500

    Resolve missing imports from merge

--- app/api/users/[user_id]/like/route.ts
+++ app/api/users/[user_id]/like/route.ts
@@ -1,3 +1,6 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
